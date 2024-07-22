41bca577accc019b503dc9804d6aedecba23ccf2
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-07-23 01:42:19 +0300

    G

--- app/strategies/page.jsx
+++ app/strategies/page.jsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import { MainButton } from "@/app/components/buttons/mainButton";
 import FlipCard from "@/app/components/flipCard/flipCard";
 import strategiesIntroImg from "@/public/assets/img/strategiesIntroImg.png";
+import rectangleImg from "@/public/assets/img/blogImages/rectangleImg.jpg";
 import arrowLeftColored from "@/public/assets/icons/arrowLeftColored.svg";
 import arrowRightColored from "@/public/assets/icons/arrowRightColored.svg";
 import UnlockPotentialContainer from "@/app/components/unlockPotentialContainer/unlockPotentialContainer";
@@ -280,30 +281,32 @@ const StrategiesPage = () => {
         </div>
 
         {/* other text */}
-        <div className="flex md:flex-row flex-col justify-center items-center w-full gap-20 my-10 md:my-40">
+        <div className="flex md:flex-row flex-col justify-between items-center w-full gap-20 my-10 md:my-40">
           {/* Text Wrapper */}
-          <div className="flex flex-col md:mb-8 mb-0 justify-between items-center md:items-start h-fit">
-            <div className="text-left flex flex-col gap-8 dark:text-white text-[#6B70ED]">
-              <p className="brightText text-wrap max-w-xl mb-4 text-3xl md:text-[44px]">
-                Strategies
-              </p>
-              <p className="whiteMainText text-wrap max-w-[35rem] text-[15px] md:text-[17px] md:leading-[25.5px] leading-5 mb-10 ">
-                DeltaPrime allows for a range of new and unique strategies.
-                Every strategy is accompanied by a practical example, getting
-                deeper into the risk and rewards of the specific strategy.
-              </p>
-            </div>
-            <div className="fullWidthButtonChildren h-[60px] md:h-full hidden my-10 w-full text-left">
-              <MainButton
-                className=""
-                label="LEARN MORE"
-                hasArrowRight={true}
-              />
+          <div className="flex flex-col items-start dark:text-white text-[#252948] mb-14 max-w-96">
+            <h4 className="uppercase mb-2 featureTitle md:text-[15px] text-[12 px] text-center text-gray-400 leading-6">
+              STRATEGIES
+            </h4>
+            <h1 className="mb-6 featureSubtitle text-[24px]">
+              Do you want to learn more about strategies?
+            </h1>
+            <p className="featureParagraph max-w-2xl text-[13px] md:leading-6 sm:text-[17px] leading-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="mt-10 md:w-fit w-full">
+              <MainButton hasArrowRight={true} label={"lEARN MORE"} />
             </div>
           </div>
           {/* Image burd */}
-          <div className="w-fit">
-            <Image src={strategiesIntroImg} alt="deltaprime_mascot_img" />
+          <div className="max-w-[650px]">
+            <Image
+              className="rounded-[20px]"
+              src={rectangleImg}
+              alt="deltaprime_mascot_img"
+            />
           </div>
         </div>
 
