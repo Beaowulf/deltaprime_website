b217026434c9a2140ebfef6a962a16379456ae88
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-07-23 01:58:09 +0300

    G

--- app/ourStory/page.jsx
+++ app/ourStory/page.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Image from "next/image";
 import { MainButton } from "@/app/components/buttons/mainButton";
 import ourStoryIntroImg from "@/public/assets/img/introOurStoryImage.png";
+import rectangleImg from "@/public/assets/img/blogImages/rectangleImg.jpg";
+import whyDeltaPrimeImg from "@/public/assets/img/whyDeltaPrimeImg.jpg";
+import UnlockPotentialContainer from "@/app/components/unlockPotentialContainer/unlockPotentialContainer";
 
 const OurStory = () => {
   return (
     <div className="sm:px-[5%] md:px-[8%] lg:px-[10%] 2xl:px-[15%] px-4">
       {/* intro */}
-      <div className="flex md:flex-row flex-col justify-center items-center w-full gap-20 my-40">
+      <div className="flex md:flex-row flex-col justify-between items-center w-full gap-20 my-40">
         {/* Text Wrapper */}
         <div className="flex flex-col md:mb-8 mb-0 justify-between items-center md:items-start h-fit">
           <div className="text-left flex flex-col gap-8 dark:text-white text-[#6B70ED]">
@@ -37,34 +40,89 @@ const OurStory = () => {
           />
         </div>
       </div>
-
+      {/* other text */}
+      <div className="flex md:flex-row flex-col justify-center items-center w-full md:gap-20 gap-5 my-10 md:my-40">
+        {/* Image burd */}
+        <div className="max-w-[600px]">
+          <Image
+            className="rounded-[20px]"
+            src={rectangleImg}
+            alt="cyberpunk_box_images"
+          />
+        </div>
+        {/* Text Wrapper */}
+        <div className="flex flex-col md:mb-8 mb-0 justify-between items-center md:items-start h-fit">
+          <div className="text-left flex flex-col gap-8 dark:text-white text-[#6B70ED]">
+            <div className="flex flex-col items-start dark:text-white text-[#252948] mt-20 mb-10">
+              <h4 className="uppercase mb-3 md:text-[15px] w-full font-bold text-[12px] text-left dark:text-gray-400 text-[#252948]">
+                Our Story
+              </h4>
+              <h1 className="mb-8 featureSubtitle md:text-[34px] text-[24px] text text-left">
+                What is DeltaPrime?
+              </h1>
+              <p className="mb-4 featureParagraph font-medium text-[13px] leading-5 md:text-[17px] md:leading-6 height max-w-xl text-left">
+                DeltaPrime is a decentralised borrowing and investing ecosystem,
+                unlocking trapped liquidity on the chains it operates on. On
+                DeltaPrime users can easily{" "}
+                <span className="underline">deposit</span> and{" "}
+                <span className="underline">borrow</span> funds to increase the
+                power of their usual DeFi investments.
+              </p>
+              <p className="mb-4 featureParagraph font-medium text-[13px] leading-5 md:text-[17px] md:leading-6 height max-w-xl text-left">
+                The <span className="underline">minimum collateral ratio</span>{" "}
+                for these loans is 20%. Borrowed funds can be used to invest in
+                <span className="underline">integrated</span> protocols.
+              </p>
+              <p className="featureParagraph font-medium text-[13px] leading-5 md:text-[17px] md:leading-6 height max-w-xl text-left">
+                Every platform can be integrated as long as they are safe and
+                liquid.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <UnlockPotentialContainer />
+      </div>
+      <div className="fullWidthButtonChildren h-[60px] md:h-full block md:hidden my-10 w-full text-center">
+        <MainButton
+          className="mx-auto"
+          label="LAUNCH APP"
+          hasArrowRight={true}
+        />
+      </div>
       {/* other text */}
       <div className="flex md:flex-row flex-col justify-center items-center w-full md:gap-20 gap-5 my-10 md:my-40">
         {/* Text Wrapper */}
         <div className="flex flex-col md:mb-8 mb-0 justify-between items-center md:items-start h-fit">
           <div className="text-left flex flex-col gap-8 dark:text-white text-[#6B70ED]">
-            <div className="flex flex-col items-center dark:text-white text-[#252948] mt-20 mb-10">
+            <div className="flex flex-col items-start dark:text-white text-[#252948] mt-20 mb-10">
               <h4 className="uppercase mb-3 md:text-[15px] w-full font-bold text-[12px] text-left dark:text-gray-400 text-[#252948]">
-                Strategies
+                Our Story
               </h4>
               <h1 className="mb-8 featureSubtitle md:text-[34px] text-[24px] text text-left">
-                Do you want to learn more about strategies?
+                Why DeltaPrime?
               </h1>
+              <p className="mb-4 featureParagraph font-medium text-[13px] leading-5 md:text-[17px] md:leading-6 height max-w-xl text-left">
+                Every year, tens of billions of dollars worth of assets are
+                locked in lending platforms. The vast majority of these funds
+                are locked as collateral, providing little to no value to the
+                crypto community.
+              </p>
               <p className="featureParagraph font-medium text-[13px] leading-5 md:text-[17px] md:leading-6 height max-w-xl text-left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                DeltaPrime returns these funds to the hands of her users. This
+                benefits borrowers, depositors, and integrated platforms alike.
               </p>
             </div>
-          </div>
-          <div className="fullWidthButtonChildren h-[60px] md:h-full hidden md:block my-10 w-full text-left">
-            <MainButton className="" label="LEARN MORE" hasArrowRight={true} />
           </div>
         </div>
         {/* Image burd */}
         <div className="w-fit">
-          <Image src={rectangleImg} alt="cyberpunk_box_images" />
+          <Image
+            className="rounded-[20px]"
+            src={whyDeltaPrimeImg}
+            alt="cyberpunk_box_images"
+          />
         </div>
       </div>
     </div>
