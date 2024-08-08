5aec3c990b6c1eabfcc623bea7ad08dce67ca3a2
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-08-08 21:30:48 +0300

    Added spin animation on ourStory section on landing page

--- app/landingPage/featureSection/aboutSection.jsx
+++ app/landingPage/featureSection/aboutSection.jsx
@@ -1,12 +1,52 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import aboutSectionIMG from "@/public/assets/img/aboutSectionIMG.png";
+import homeAvalanche from "@/public/assets/img/images/home-avalanche.png";
+import homeArbitrum from "@/public/assets/img/images/home-arbitrum.png";
 import { AboutButtonDarkBG } from "@/app/components/buttons/mainButton";
+
+const ImageComponent = () => {
+  const [showFirstImage, setShowFirstImage] = useState(true);
+  const [initialSpin, setInitialSpin] = useState(true);
+
+  useEffect(() => {
+    const initialTimeout = setTimeout(() => {
+      setInitialSpin(false);
+      setShowFirstImage((prev) => !prev);
+      // Set an interval to continue spinning every 5 seconds
+      const interval = setInterval(() => {
+        setShowFirstImage((prev) => !prev);
+      }, 6000);
+
+      return () => clearInterval(interval);
+    }, 1200); // Initial spin for 1500ms
+
+    return () => clearTimeout(initialTimeout);
+  }, []);
+
+  return (
+    <div className="content-image__wrapper spinner">
+      <Image
+        className={`content-image ${
+          showFirstImage ? "" : "content-image--hidden"
+        }`}
+        src={homeAvalanche}
+        alt="DeltaPrime"
+      />
+      <Image
+        className={`content-image ${
+          showFirstImage ? "content-image--hidden" : ""
+        }`}
+        src={homeArbitrum}
+        alt="DeltaPrime"
+      />
+    </div>
+  );
+};
 
 function AboutSection() {
   return (
-    <div className="aboutSectionBG flex flex-wrap items-center justify-between bg-cover bg-no-repeat px-4 pb-10 sm:px-6 md:px-[8%] lg:px-[10%] xl:px-[16%] 2xl:px-[20%]">
+    <div className="aboutSectionBG flex flex-wrap items-center justify-between bg-cover bg-no-repeat px-4 pb-10 sm:px-6 md:px-[8%] lg:px-[10%] xl:px-[15%] ">
       {/* Left Side */}
       <div className="w-full md:w-1/2">
         <h4 className="pt-24  aboutTypographytitle">OUR STORY</h4>
@@ -28,7 +68,7 @@ function AboutSection() {
       {/* Right Side */}
       <div className="px-10 pt-10 w-full md:w-1/2 flex justify-center">
         <div className="w-fit">
-          <Image className="" src={aboutSectionIMG} alt="about_section_img" />
+          <ImageComponent />
         </div>
       </div>
     </div>
