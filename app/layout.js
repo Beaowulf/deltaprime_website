1efe9fb30913bbcc1574fcaf43e358dcf08ff943
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-08-30 11:02:54 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,8 @@ const montserrat = Montserrat({
 export const metadata = {
   title: "Delta Prime",
   description: "Delta Prime Web App",
+  name: "robots",
+  content: "noindex,nofollow",
 };
 
 export default function RootLayout({ children }) {
