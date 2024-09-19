82e281b007aa3b1b285ec9f2be5315599c593b63
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-09-19 21:37:33 +0300

    Update launchAppModal.jsx

--- app/components/modals/launchAppModal.jsx
+++ app/components/modals/launchAppModal.jsx
@@ -42,7 +42,7 @@ const LaunchAppModal = () => {
       {modal && (
         <dialog className="modalP fixed left-0 top-0 w-full h-full bg-black bg-opacity-50  overflow-auto backdrop-blur flex justify-center items-center">
           <div className="modalParent relative w-full md:w-fit">
-            <div className="absolute top-2 right-4 pb-2 pt-10 pl-5 pr-5 w-fit h-fit text-black cursor-pointer z-50">
+            <div className="absolute top-2 right-4 pb-2 pt-20 pl-5 pr-5 w-fit h-fit text-black cursor-pointer z-50">
               <Link
                 className="z-100 cursor-pointer"
                 href={pathname}
@@ -105,8 +105,8 @@ const LaunchAppModal = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="">
-                      <div className="flex justify-center items-center pb-5">
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full">
+                      <div className="flex justify-center items-center pb-4">
                         <Link
                           className="mx-auto"
                           target="_blank"
@@ -129,7 +129,7 @@ const LaunchAppModal = () => {
 
                 {/* Right side */}
                 <div className="flex justify-center flex-col gap-1 items-center modalWrapper md:w-full h-[16rem] w-[17rem]">
-                  <div className="modalContent w-full h-full">
+                  <div className="modalContent w-full h-full re">
                     <div className="w-full bg-[#6B70ED] py-1 rounded-t-[25px] mt-[0.5px] mx-[0.25px] mb-2">
                       <p className="mx-auto w-fit text-white font-bold">
                         Prime Account
@@ -152,7 +152,7 @@ const LaunchAppModal = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="">
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2  w-full">
                       <div className="flex justify-center items-center pb-4">
                         <Link
                           className="mx-auto"
