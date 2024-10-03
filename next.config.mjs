81365f43e11fa2c88fbf547c3fd14f06a58fcffb
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-03 13:18:54 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -36,7 +36,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.google.com; font-src 'self' data: https://your-font-host.com; connect-src 'self'; frame-src 'self' https://www.youtube.com",
+              "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://www.google.com https://www.googletagmanager.com; font-src 'self' data: https://your-font-host.com; img-src 'self' https://images.ctfassets.net data:; connect-src 'self' https://oracle-gateway-2.a.redstone.finance https://2t8cq5jra.execute-api.us-east-1.amazonaws.com https://cdn.contentful.com; frame-src 'self' https://www.youtube.com;",
           },
           {
             key: "X-Content-Type-Options",
