f75521f119567510d50a6df1a5a39125fd24389f
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-05 21:57:12 +0300

    Update contactForm.jsx

--- app/ui/contactForm/contactForm.jsx
+++ app/ui/contactForm/contactForm.jsx
@@ -9,89 +9,26 @@ import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = ({ hasUnlockPotentialContainer = true }) => {
   const { resolvedTheme } = useTheme();
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
   const [isSubmitting, setIsSubmitting] = useState(false); // Add loading state
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-
-  const resetForm = () => {
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-    });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!formData.name || !formData.email || !formData.message) {
-      toast.error("All fields are required.", {
+    // Manually trigger a success message
+    toast.success(
+      "Thanks for reaching out. A member of our team will review your message and contact you shortly.",
+      {
         position: "top-right",
-        autoClose: 5000,
+        autoClose: 6000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
+        progress: undefined,
         theme: resolvedTheme === "dark" ? "dark" : "light",
-      });
-      return;
-    }
-
-    setIsSubmitting(true); // Set loading state
-    try {
-      const response = await fetch(
-        "https://formsubmit.co/contact@deltaprime.io",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: formData.name,
-            email: formData.email,
-            message: formData.message,
-          }),
-        }
-      );
-
-      if (response.ok) {
-        toast.success(
-          "Thanks for reaching out. A member of our team will review your message and contact you shortly.",
-          {
-            position: "top-right",
-            autoClose: 6000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: resolvedTheme === "dark" ? "dark" : "light",
-          }
-        );
-        resetForm(); // Reset the form after submission
-      } else {
-        throw new Error("Form submission failed");
       }
-    } catch (error) {
-      toast.error("Something went wrong. Please try again later.", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: resolvedTheme === "dark" ? "dark" : "light",
-      });
-    } finally {
-      setIsSubmitting(false); // Reset loading state
-    }
+    );
+    setIsSubmitting(true); // Set loading state
   };
 
   return (
@@ -120,19 +57,26 @@ const ContactForm = ({ hasUnlockPotentialContainer = true }) => {
         </div>
         {/* Right Side */}
         <div className="flex-1 flex flex-col justify-center p-8">
-          <form className="w-full max-w-lg mx-auto" onSubmit={handleSubmit}>
+          <form
+            className="w-full max-w-lg mx-auto"
+            action="https://formsubmit.co/contact@deltaprime.io"
+            method="POST"
+            onSubmit={handleSubmit}
+          >
             {/* Hidden Inputs for FormSubmit Configuration */}
             <input type="hidden" name="_captcha" value="false" />
+            <input type="hidden" name="_next" value="https://deltaprime.io" />
             <input
               type="hidden"
               name="_url"
-              value="https://deltaprime.io/contact-us.html"
+              value="https://deltaprime.io/contact"
             />
 
             <div className="flex gap-5">
               <div className="mb-4 flex-1">
                 <label
-                  className="block dark:text-white text-[#6B70ED] font-bold mb-2 text-[12px] md:text-[17px]"
+                  className="block dark:text-white text-[#6B70ED
+                font-bold mb-2 text-[12px] md:text-[17px]"
                   htmlFor="name"
                 >
                   Name
@@ -140,10 +84,9 @@ const ContactForm = ({ hasUnlockPotentialContainer = true }) => {
                 <input
                   className="shadow appearance-none border-2 rounded-lg w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline border-[#7C71FF] bg-transparent focus:border-[#B39FFF]"
                   id="name"
+                  name="name" // Added name attribute for form submission
                   type="text"
                   placeholder="Your Name"
-                  value={formData.name}
-                  onChange={handleChange}
                   required
                   disabled={isSubmitting} // Disable input during submission
                 />
@@ -158,10 +101,9 @@ const ContactForm = ({ hasUnlockPotentialContainer = true }) => {
                 <input
                   className="shadow appearance-none border-2 rounded-lg w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline border-[#7C71FF] bg-transparent focus:border-[#B39FFF]"
                   id="email"
+                  name="email" // Added name attribute for form submission
                   type="email"
                   placeholder="Your Email"
-                  value={formData.email}
-                  onChange={handleChange}
                   required
                   disabled={isSubmitting} // Disable input during submission
                 />
@@ -178,9 +120,8 @@ const ContactForm = ({ hasUnlockPotentialContainer = true }) => {
               <textarea
                 className="shadow appearance-none border-2 rounded-lg w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline h-32 resize-none border-[#7C71FF] bg-transparent focus:border-[#B39FFF]"
                 id="message"
+                name="message" // Added name attribute for form submission
                 placeholder="Your Message"
-                value={formData.message}
-                onChange={handleChange}
                 required
                 disabled={isSubmitting} // Disable input during submission
               ></textarea>
