8201c30cc72e126bca78d4d7db04bc66cbe60449
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-15 20:34:36 +0300

    Changed to get the first 3 blogs

--- app/landingPage/index.jsx
+++ app/landingPage/index.jsx
@@ -4,85 +4,49 @@ import { documentToPlainTextString } from "@contentful/rich-text-plain-text-rend
 import { fetchTvlData } from "@/lib/getCryptoData";
 import { fetchLandingData } from "@/lib/getLandingData";
 
-// Utility function to format ISO date string
-function formatDateString(isoString, locale = "en-GB") {
-  const date = new Date(isoString);
-  return date.toLocaleDateString(locale, {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-}
-
 function countWords(str) {
   // Split by space or any non-word character to correctly count words
   return str.split(/\s+/).filter((word) => word !== "").length;
 }
 
-// This is to get a random blog of each category (so that landing page is populated with different blogs each time etc)
-function getRandomItem(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-
 // HomePage component
 const HomePage = async () => {
-  // Fetch blogs data
   const blogs = await fetchBlogs();
-  const landingData = await fetchLandingData(); // Fetch landing data
+  const landingData = await fetchLandingData();
 
   if (!landingData) {
     return <div>Error fetching landing data.</div>;
   }
 
-  // Get the sectionsCollection from landingData
   const { sectionsCollection } = landingData;
 
-  // Get and Process blog data to match what we need for the previewCards on the landing page
-  const blogsByCategory = blogs.reduce((acc, blog) => {
-    const blogCategory = blog.blogCategory;
-    if (!acc[blogCategory]) {
-      acc[blogCategory] = [];
-    }
-    acc[blogCategory].push(blog);
-    return acc;
-  }, {});
+  const selectedBlogs = blogs.slice(0, 3);
 
-  const blogPreviewCardData = Object.keys(blogsByCategory).map((category) => {
-    const categoryBlogs = blogsByCategory[category];
-    const randomBlog = getRandomItem(categoryBlogs);
-
-    // Process the blog data
-    const processBlog = (blog) => {
-      const description = blog.blogDescription;
-      const paragraphs = documentToPlainTextString(blog.blogRichTextParagraph);
-      const wordCount = countWords(paragraphs);
-      const minsToRead = Math.ceil(wordCount / 210);
-      const blogID = blog.blogID;
-
-      return {
-        ...blog,
-        description,
-        minsToRead,
-        blogID: blogID, // Ensure we have a blog ID for linking
-      };
-    };
+  const processBlog = (blog) => {
+    const description = blog.blogDescription;
+    const paragraphs = documentToPlainTextString(blog.blogRichTextParagraph);
+    const wordCount = countWords(paragraphs);
+    const minsToRead = Math.ceil(wordCount / 210);
+    const blogID = blog.blogID;
 
     return {
-      category,
-      blog: processBlog(randomBlog),
+      ...blog,
+      description,
+      minsToRead,
+      blogID,
     };
-  });
+  };
 
-  // Convert preview card data to array
-  const previewDataArray = Object.values(blogPreviewCardData);
+  // Process the first 3 blogs
+  const blogPreviewCardData = selectedBlogs.map(processBlog);
+
   const tvlData = await fetchTvlData();
 
   return (
     <>
       <LandingPage
         totalTvl={tvlData.totalTvl}
-        blogPreviewCardData={previewDataArray}
+        blogPreviewCardData={blogPreviewCardData}
         sectionsCollection={sectionsCollection} // Pass sectionsCollection to LandingPage
       />
     </>
