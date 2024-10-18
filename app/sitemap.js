9ca61c3c53adc99ecaf9c85664205bb6fd025ea4
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-18 15:14:42 +0300

    Updated sitemap and robots files (deleted the 2 txt ones)

--- app/sitemap.js
+++ app/sitemap.js
@@ -8,8 +8,9 @@ export async function sitemap() {
   const howToVideos = await fetchHowToVideos(); // Fetch how-to videos
   const strategies = await fetchStrategies(); // Fetch strategies
   const jobDescriptions = await getJobDescriptions(); // Fetch job descriptions
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://deltaprime.io';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://deltaprime.io";
 
+  // todo: doublecheck this.
   // Generate sitemap entries for each blog
   const blogUrls = blogs.map((blog) => ({
     url: `${baseUrl}/blogs/academy/${blog.slug}`,
@@ -42,15 +43,34 @@ export async function sitemap() {
     { url: `${baseUrl}/tokenomics`, lastModified: new Date().toISOString() },
     { url: `${baseUrl}/strategies`, lastModified: new Date().toISOString() },
     { url: `${baseUrl}/blogs`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/blogs/academy/glossary`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/legals/terms-Of-Use`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/legals/privacy-And-Policy`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/legals/tokenomics-Risk-Disclaimer`, lastModified: new Date().toISOString() },
-
+    {
+      url: `${baseUrl}/blogs/academy/glossary`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/legals/terms-Of-Use`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/legals/privacy-And-Policy`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/legals/tokenomics-Risk-Disclaimer`,
+      lastModified: new Date().toISOString(),
+    },
 
     // Add other static pages here
   ];
 
   // Return the combined list of URLs
-  return [...staticRoutes, ...blogUrls, ...howToVideoUrls, ...strategyUrls, ...jobDescriptionUrls];
+  return [
+    ...staticRoutes,
+    ...blogUrls,
+    ...howToVideoUrls,
+    ...strategyUrls,
+    ...jobDescriptionUrls,
+  ];
 }
+
+export default sitemap;
