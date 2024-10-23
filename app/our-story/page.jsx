a1fea7acbac8023275923bb814f04fae3fabec49
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-23 20:06:37 +0300

    Updated our-story section

--- app/our-story/page.jsx
+++ app/our-story/page.jsx
@@ -1,7 +1,8 @@
 import OurStoryClient from "./ourStoryClient"; // Client component for rendering
 import { fetchTvlData } from "@/lib/getCryptoData";
 import { getOurStorySections } from "@/lib/ourStoryData"; // Fetching the Our Story data
-import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'; // Convert rich text JSON to plain text
+import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer"; // Convert rich text JSON to plain text
+import { getJobDescriptions } from "@/lib/jobData";
 
 // Define dynamic metadata for this page
 export async function generateMetadata() {
@@ -28,25 +29,22 @@ export async function generateMetadata() {
 }
 
 export default async function OurStoryPage() {
-  console.log("OurStoryPage: Fetching our story data...");
+  // console.log("OurStoryPage: Fetching our story data...");
 
   // Fetch the Our Story sections data on the server side
   const storyData = await getOurStorySections();
-  console.log(storyData);
-
-
-
+  // console.log(storyData);
 
   // Fetch TVL (Total Value Locked) data
   const tvlData = await fetchTvlData();
   const tvtDataFormatted = tvlData.totalTvl.slice(0, 2);
 
   if (!storyData) {
-    console.log("Error: No Our Story data found");
+    // console.log("Error: No Our Story data found");
     return <div>Error: Unable to fetch Our Story data.</div>;
   }
 
-  console.log("OurStoryPage: Our Story data received:", storyData);
+  // console.log("OurStoryPage: Our Story data received:", storyData);
 
   // Pass the fetched data to the client component
   return (
