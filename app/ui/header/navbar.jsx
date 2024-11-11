f366f50d8c258bd6fe2eb4beb8f51a65c966184d
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-11-11 14:15:54 +0200

    Added banner

--- app/ui/header/navbar.jsx
+++ app/ui/header/navbar.jsx
@@ -21,7 +21,8 @@ import MegaMenu from "@/app/ui/megaMenu";
 function Nav() {
   const [isOpenMobile, setIsOpenMobile] = useState(false);
   const [isOpenDesktop, setIsOpenDesktop] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false); // Define isScrolled state here
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isBannerVisible, setIsBannerVisible] = useState(true);
   const pathname = usePathname();
   const { resolvedTheme } = useTheme();
 
@@ -51,7 +52,9 @@ function Nav() {
     <>
       {/* Main Navbar */}
       <div
-        className={`pagePaddingMedium ${
+        className={`pagePaddingMedium relative ${
+          isBannerVisible ? "pt-16 md:pt-10" : "pt-0"
+        }  ${
           isScrolled
             ? isOpenDesktop
               ? "sticky top-0 bg-transparent z-[1000] transition-all duration-300"
@@ -64,6 +67,34 @@ function Nav() {
             isScrolled && "py-0 pt-0"
           }`}
         >
+          <div
+            className={`w-full left-0 top-0 absolute bg-[#F4D35E] text-black dark:text-white dark:bg-[#BA6223] z-[100000000] ${
+              isBannerVisible ? "block" : "hidden"
+            }`}
+          >
+            <div className="flex justify-center items-center px-4 py-3 relative">
+              <p className="text-center font-semibold text-[14px] md:text-[16px] mr-5 lg:mr-0">
+                DeltaPrime is currently paused due to an attack on the Saving
+                pools.{" "}
+                <a
+                  href="https://discord.com/channels/889510301421166643/912702114252329060/1305455825023664168"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="cursor-pointer underline hover:no-underline font-bold"
+                >
+                  Read More.
+                </a>
+              </p>
+              <button
+                onClick={() => setIsBannerVisible(false)}
+                aria-label="Close banner"
+                className="absolute right-4 text-[20px] text-black dark:text-white hover:text-gray-700 dark:hover:text-gray-300 md:mr-10 font-semibold"
+              >
+                X
+              </button>
+            </div>
+          </div>
+
           <div className="flex items-center justify-between h-20">
             {/* Logo and Theme Toggle */}
             <div className="flex items-center gap-2">
